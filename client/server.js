7c3f7c50d8be7ee047e8611cb2d455d9ae3d6a51
olepychev <asmakech115@gmail.com>
2023-07-03 01:45:30 +0900

    .

--- client/server.js
+++ client/server.js
@@ -4,15 +4,6 @@ import { handler } from "./build/handler.js";
 
 app.use(express.static('public'));
 
-app.use((req, res, next) => {
-  const host = req.get('host');
-  if (host === '172.104.111.61:10002') {
-    res.redirect('https://demo.goodfriendsgaming.com');
-  } else {
-    next();
-  }
-});
-
 app.use(handler);
 
 const port = 10002;
