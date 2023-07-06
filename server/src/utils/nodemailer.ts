a0c00e4876af4bfd3c2b5801d5f2c1b084c4f614
olepychev <asmakech115@gmail.com>
2023-07-07 00:00:25 +0900

    smtp -> smtp2go

--- server/src/utils/nodemailer.ts
+++ server/src/utils/nodemailer.ts
@@ -1,24 +1,19 @@
 import nodemailer from "nodemailer";
-
-const sendEmail = (email:string, code:string) => {
+const sendEmail = (email:string, code:string) => {  
   try {
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: { 
-          user: process.env.NODEMAILER_EMAIL, 
-          pass: process.env.NODEMAILER_PASSWORD
-      }
-    })
-
+    const transporter = nodemailer.createTransport({      host: 'mail.smtp2go.com',
+      port: 2525,      
+      auth: {
+        user: 'info@goodfriendsgaming.com',        pass: 'rnfptskfn1!'
+      }    
+    });
     const mailOptions = {
-      to: email,
+      to: email,      
       subject: 'Email subscription verification code',
-      html: `Please enter your verification code. ${code}`,
-    }
-    return transporter.sendMail(mailOptions)
+      html: `Please enter your verification code. ${code}`,    }
+    return transporter.sendMail(mailOptions)  
   } catch (error) {
-    console.log(error)
+    console.log(error)  
   }
 }
-
 export {sendEmail}
