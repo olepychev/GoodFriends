b96573dd7261a361431d4939beb93cff785949e0
olepychev <asmakech115@gmail.com>
2023-07-13 21:34:36 +0900

    add dynamic origin

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,8 +9,8 @@ if(process.env.NODE_ENV === "production") {
     dotenv.config({ path: path.resolve(__dirname, '../.env.development')});
 }
 
-import express, { Application, Request, Response } from 'express';
-import cors from 'cors';
+import express, { Application, Request, Response, NextFunction } from 'express';
+import cors, { CorsOptions } from 'cors';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 
@@ -18,23 +18,6 @@ const app: Application = express()
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: [
-        "http://localhost:10010", 
-        "https://demo.goodfriendsgaming.com",
-        "45.76.148.155",
-        "45.77.174.118",
-        "45.76.185.1",
-        "45.77.248.182",
-        "45.76.179.104",
-        "45.76.179.39",
-        "45.76.160.35",
-        "52.74.15.8",
-        "139.180.209.126"],
-    methods: ["GET", "POST"],
-    credentials: true 
-}));
-
 // app.use(cors({
 //     origin: [
 //         "http://localhost:10010", 
@@ -51,6 +34,40 @@ app.use(cors({
 //     methods: ["GET", "POST"],
 //     credentials: true 
 // }));
+
+const allowedOriginsWithCredentials: string[] = [
+    "http://localhost:10010", 
+    "https://demo.goodfriendsgaming.com"];
+
+const allowedOriginsWithoutCredentials: string[] = [
+    "45.76.148.155",
+    "45.77.174.118",
+    "45.76.185.1",
+    "45.77.248.182",
+    "45.76.179.104",
+    "45.76.179.39",
+    "45.76.160.35",
+    "52.74.15.8",
+    "139.180.209.126"];
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    const origin: string | undefined = req.header('Origin');
+    let corsOptions: CorsOptions;
+
+    if (origin) {
+        if (allowedOriginsWithCredentials.includes(origin)) {
+            corsOptions = { origin: true, credentials: true };
+        } else if (allowedOriginsWithoutCredentials.includes(origin)) {
+            corsOptions = { origin: true, credentials: false };
+        } else {
+            corsOptions = { origin: false, credentials: false };
+        }
+    } else {
+        corsOptions = { origin: false, credentials: false };
+    }
+
+    cors(corsOptions)(req, res, next);
+});
 
 import * as middlewares from './middlewares/validation.middlewares';
 app.use("/api/*", middlewares.checkHeaders);
