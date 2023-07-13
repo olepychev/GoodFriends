5bf5bcc915be2eb7e58275fdc013176169897fef
olepychev <asmakech115@gmail.com>
2023-07-13 17:30:17 +0900

    add honor-link ip

--- server/src/server.ts
+++ server/src/server.ts
@@ -19,7 +19,18 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:10010", "https://demo.goodfriendsgaming.com"],
+    origin: [
+        "http://localhost:10010", 
+        "https://demo.goodfriendsgaming.com",
+        "45.76.148.155",
+        "45.77.174.118",
+        "45.76.185.1",
+        "45.77.248.182",
+        "45.76.179.104",
+        "45.76.179.39",
+        "45.76.160.35",
+        "52.74.15.8",
+        "139.180.209.126"],
     methods: ["GET", "POST"],
     credentials: true 
 }));
