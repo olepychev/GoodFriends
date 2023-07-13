9abd69e4da401107c34cb487616afce28628ea1a
olepychev <asmakech115@gmail.com>
2023-07-13 16:08:20 +0900

    casino test

--- server/src/models/casino.models.ts
+++ server/src/models/casino.models.ts
@@ -1,5 +1,18 @@
 // change balance
 export const casinoHistoryInsert = async (dataAccess: any, affiliateCode: string, memberIdx: number, nick: string, transactionId: number, transactionType: string, refererId: number, amount: number, gameId: number, gameTitle: string, gameType: string, gameVendor: string)  => {
+    console.log(
+        affiliateCode, 
+        memberIdx, 
+        nick, 
+        transactionId, 
+        transactionType, 
+        refererId, 
+        amount, 
+        gameId, 
+        gameTitle,
+        gameType, 
+        gameVendor)
+    
     const sql: string = `
         INSERT INTO gf_casino_history
             SET affiliate_code = ?,
