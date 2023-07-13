d37d0527a29a7daa0a741d15ddd267d0ecbcf925
olepychev <asmakech115@gmail.com>
2023-07-13 21:53:11 +0900

    .

--- server/src/middlewares/casino.middlewares.ts
+++ server/src/middlewares/casino.middlewares.ts
@@ -16,8 +16,8 @@ export const queryUserCheck = async (req: Request, res: Response, next: NextFunc
 export const postUserCheck = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const {username} = req.body
     const check = await dataAccess.findOne(`gf_member`, `nick`, {column: "nick", condition: "=", data: username})
-
-    console.log(check, username)
+    
+    console.log(req.body)
 
     if(check) {
         next()
