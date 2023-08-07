f559480deebf2b318dda6ac3e5e6ad344f9f8d3b
olepychev <asmakech115@gmail.com>
2023-08-07 19:10:56 +0900

    delete client

--- client/src/routes/casino/+page.server.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('./$types').PageLoad} */
-
-export async function load({url}) {
-    const data = Object.fromEntries(
-    	new URLSearchParams(url.toJSON().split("?")[1])
-    );
-	return data;
-}
